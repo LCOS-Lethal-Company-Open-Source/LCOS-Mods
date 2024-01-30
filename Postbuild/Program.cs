f76ede175b6c1150c5b34deae7cc676d8a09aae9
Floofer++ <56936160+dylanrafael05@users.noreply.github.com>
2024-01-30 17:47:05 -0500

    First working version of tooling

---
+++ Postbuild/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+// CONSTANTS //
+const string LETHAL_LOCATION = @"C:\Program Files (x86)\Steam\steamapps\common\Lethal Company";
+const string PLUGINS_LOCATION = LETHAL_LOCATION + @"\BepInEx\plugins";
+
+// SANITY CHECKS //
+if(!Directory.Exists(LETHAL_LOCATION)) {
+    Console.Error.WriteLine(":: ERROR ::");
+    Console.Error.WriteLine("Please download Lethal Company before attempting to mod it!");
+    Environment.Exit(1);
+}
+
+if(!Directory.Exists(PLUGINS_LOCATION)) {
+    Console.Error.WriteLine(":: ERROR ::");
+    Console.Error.WriteLine("Please install BepInEx before building your mods!");
+    Environment.Exit(1);
+}
+
+// --DEV //
+if(args[0] == "--dev") {
+    // Copy compilation output to lethal company //
+    Console.WriteLine($":: LOG :: - Running copy from {args[1]}");
+
+    // TODO: better error handling
+
+    var mods = Directory.GetFiles(args[1], "*.dll", SearchOption.AllDirectories)
+        .Where(x => File.Exists(Path.ChangeExtension(x, ".pdb")));
+
+    var outputLocs = new[] {
+        Directory.CreateDirectory(PLUGINS_LOCATION),
+        Directory.CreateDirectory(Path.Combine(args[2], "build"))
+    };
+
+    foreach(var mod in mods) {
+        foreach(var outputLoc in outputLocs) {
+            var newfile = Path.Combine(outputLoc.FullName, Path.GetFileName(mod));
+            if(File.Exists(newfile)) {
+                Console.WriteLine($":: LOG :: - Removing old version of {newfile}");
+                File.Delete(newfile);
+            }
+
+            Console.WriteLine($":: LOG :: - Copying to {newfile}");
+            File.Copy(mod, newfile);
+        }
+    }
+
+    Console.WriteLine($":: LOG :: - Completed copying to plugin location");
+
+    // Run lethal company if requested //
+    if(args.Length > 3 && args[3] == "--run") {
+        new Process {
+            StartInfo = new ProcessStartInfo {
+                FileName = Path.Combine(LETHAL_LOCATION, "Lethal Company.exe"),
+                UseShellExecute = true
+            }
+        }.Start();
+    }
+}
--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 This repository is maintained by a dedicated team through the Rensselaer Center for Open Source (RCOS). We develop mods for Lethal Company, aiming to enhance and diversify the gaming experience.
 
+## Repository Setup
+
+This repository is comprised of one large `.sln`, which holds many `.csproj` files which are either mods or modding tools. The `Postbuild` folder holds a helper script which will simplify the process of setting up and testing mods. This folder is referenced by the `.csproj` of `Template`, which serves as a base for mod creation. This folder can be copied to create new mods, but these copies must be placed in the root of this repository.
+
 ## Community and Support
 
 For discussions, support, and sharing ideas about our mods:
