584a78280bba55085a675eb00888326ae03963b6
Floofer++ <56936160+dylanrafael05@users.noreply.github.com>
2024-02-02 17:06:38 -0500

    Improved tooling, created unity proj

--- Postbuild/Program.cs
+++ Postbuild/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO.Compression;
 using System.Linq;
 
 // CONSTANTS //
 const string LETHAL_LOCATION = @"C:\Program Files (x86)\Steam\steamapps\common\Lethal Company";
 const string PLUGINS_LOCATION = LETHAL_LOCATION + @"\BepInEx\plugins";
+const string THUNDERSTORE_FOLDER = "thunderstore";
 
 // SANITY CHECKS //
 if(!Directory.Exists(LETHAL_LOCATION)) {
@@ -25,29 +27,62 @@ if(args[0] == "--dev") {
     Console.WriteLine($":: LOG :: - Running copy from {args[1]}");
 
     // TODO: better error handling
+    var projLoc = args[2];
+    var depsLoc = Path.Combine(projLoc, "deps");
 
-    var mods = Directory.GetFiles(args[1], "*.dll", SearchOption.AllDirectories)
+    var filesToCopy = Directory.GetFiles(args[1], "*.dll", SearchOption.AllDirectories)
         .Where(x => File.Exists(Path.ChangeExtension(x, ".pdb")));
 
+    // Add dependencies to copy
+    if(Path.Exists(depsLoc)) {
+        filesToCopy = filesToCopy.Concat(Directory.GetFiles(depsLoc));
+    }
+
+    var buildLoc = Directory.CreateDirectory(Path.Combine(projLoc, "build"));
+
+    foreach(var buildFile in buildLoc.GetFiles()) {
+        Console.WriteLine($":: LOG :: - Removing old version of {buildFile.FullName}");
+        buildFile.Delete();
+    }
+    
     var outputLocs = new[] {
         Directory.CreateDirectory(PLUGINS_LOCATION),
-        Directory.CreateDirectory(Path.Combine(args[2], "build"))
+        buildLoc
     };
 
-    foreach(var mod in mods) {
+    foreach(var fileToCopy in filesToCopy) {
         foreach(var outputLoc in outputLocs) {
-            var newfile = Path.Combine(outputLoc.FullName, Path.GetFileName(mod));
-            if(File.Exists(newfile)) {
-                Console.WriteLine($":: LOG :: - Removing old version of {newfile}");
+            var newfile = Path.Combine(outputLoc.FullName, Path.GetFileName(fileToCopy));
+
+            if(Path.Exists(newfile)) {
                 File.Delete(newfile);
             }
 
             Console.WriteLine($":: LOG :: - Copying to {newfile}");
-            File.Copy(mod, newfile);
+            File.Copy(fileToCopy, newfile);
         }
     }
 
     Console.WriteLine($":: LOG :: - Completed copying to plugin location");
+
+    // Build thunderstore output
+    var thunderstoreLoc = Path.Combine(projLoc, THUNDERSTORE_FOLDER);
+    var thunderstoreFiles = new List<FileInfo>();
+    thunderstoreFiles.AddRange(Directory.CreateDirectory(thunderstoreLoc).GetFiles());
+    thunderstoreFiles.AddRange(buildLoc.GetFiles());
+    
+    var zip = Path.Combine(buildLoc.FullName, "thunderstore.zip");
+    if(Path.Exists(zip)) {
+        File.Delete(zip);
+    }
+    
+    Console.WriteLine($":: LOG :: - Creating zip file for thunderstore release");
+
+    using(var zipfile = ZipFile.Open(zip, ZipArchiveMode.Create)) {
+        foreach(var file in thunderstoreFiles) {
+            zipfile.CreateEntryFromFile(file.FullName, file.Name);
+        }
+    }
 
     // Run lethal company if requested //
     if(args.Length > 3 && args[3] == "--run") {
--- README.md
+++ README.md
@@ -6,6 +6,8 @@ This repository is maintained by a dedicated team through the Rensselaer Center 
 
 This repository is comprised of one large `.sln`, which holds many `.csproj` files which are either mods or modding tools. The `Postbuild` folder holds a helper script which will simplify the process of setting up and testing mods. This folder is referenced by the `.csproj` of `Template`, which serves as a base for mod creation. This folder can be copied to create new mods, but these copies must be placed in the root of this repository.
 
+To create Unity asset bundles, use the provided unity project. Create a folder for your mod's assets, assign each of your assets to its own bundle, and then run `Assets/Create AssetBundles`. The bundles will be saved to the `UnityProject/AssetBundles` folder, where they can be copied into your mod folders.
+
 ## Community and Support
 
 For discussions, support, and sharing ideas about our mods:
