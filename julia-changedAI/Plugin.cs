21dd81e08f54e2fb321970757194a0aba8922009
jkpch <127340814+jkpch@users.noreply.github.com>
2024-03-18 13:20:48 -0400

    Created a small mod that drops items upon jump

---
+++ julia-changedAI/Plugin.cs
@@ -0,0 +1,34 @@
+﻿﻿using System;
+using BepInEx;
+using GameNetcodeStuff;
+using HarmonyLib;
+using UnityEngine;
+namespace ChangedAI;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+
+    Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+
+    private void Awake()
+    {
+        // Plugin load logic goes here!
+        // This script acts like a unity object.
+        Logger.LogInfo($"What up gamers");
+        harmony.PatchAll();
+
+    }
+
+    //[HarmonyPatch(typeof(PlayerControllerB), "FallValue")]
+    [HarmonyPatch(typeof(PlayerControllerB), "PlayerJump")]
+    class Patch
+    {
+        static bool Prefix(ref PlayerControllerB __instance)
+        {
+            __instance.jumpForce = 17f;
+            __instance.DropAllHeldItems();
+            return true;
+        }
+    }  
+}
---
+++ julia-changedAI/thunderstore/README.md
@@ -0,0 +1,6 @@
+# Changed Mob AI
+
+Goal is to add some simple changed to mob behavior, such as changing movement speed or how they see. 
+
+Mod by @kapchj on github.
+Default icon by @lilujk on github.
