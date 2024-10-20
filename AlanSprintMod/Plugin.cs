c19588ed4e54ce4cf1ab024a66524075a8243463
Alan Weng <115497296+akkilin@users.noreply.github.com>
2024-10-20 19:51:40 -0400

    Fixed plugin.cs

--- AlanSprintMod/Plugin.cs
+++ AlanSprintMod/Plugin.cs
@@ -1,13 +1,48 @@
-﻿﻿using BepInEx;
-namespace Template;
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using LCInfSpr.Patches;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
-public class Plugin : BaseUnityPlugin
+namespace LCInfSpr
 {
-    private void Awake()
+    [BepInPlugin(modGUID,modName,modVersion)]
+    public class TutMod: BaseUnityPlugin
     {
-        // Plugin load logic goes here!
-        // This script acts like a unity object.
-        Logger.LogInfo($"Hello World!");
+        private const string modGUID = "Alan.LCInfSpr";
+        private const string modName = "Tut. Infinite sprint";
+        private const string modVersion = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static TutMod Instance;
+
+        internal ManualLogSource logSource;
+
+        void Awake() 
+        {
+            if (Instance == null) 
+            { 
+            Instance = this;
+            }
+            
+
+            // logging
+            logSource = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            
+
+            logSource.LogInfo("Tutorial mod is active");
+
+            // patch this into game
+            harmony.PatchAll(typeof(TutMod));
+            harmony.PatchAll(typeof(PlayerControllerBPatch));
+
+        }
+
+
     }
 }
