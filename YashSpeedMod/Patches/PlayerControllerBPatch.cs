2b483ab906630196e89edbb95c85009869f9ebe9
yashmistri <itsyashmistri@gmail.com>
2024-11-02 22:39:48 -0400

    Add YashSpeedMod

---
+++ YashSpeedMod/Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,21 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YashSpeedMod.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerBPatch
+    {
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void SpeedPatch(ref float ___movementSpeed)
+        {
+            ___movementSpeed = 0f;
+        }
+    }
+}
