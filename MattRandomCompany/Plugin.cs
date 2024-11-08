4ea8fcf98dd2473ddd39238e20077d33feb24ae4
M-Partridge <partrm2@rpi.edu>
2024-11-08 18:10:26 -0500

    First commit for random company

---
+++ MattRandomCompany/Plugin.cs
@@ -0,0 +1,50 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomCompany
+{
+    /*
+     * What is randomized?
+     *  Scrap spawn rates (.5x to 2x scrap)
+     *  Scrap values
+     * 
+     */
+
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class RandomCompany : BaseUnityPlugin
+    {
+        private const string modGUID = "M-Partridge.RandomCompany";
+        private const string modName = "Random Company";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static RandomCompany instance;
+
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo(modName + " has started");
+
+            harmony.PatchAll();
+        }
+        public static void logMessage(string message)
+        {
+            instance.mls.LogInfo(modName + " : " + message);
+        }
+    }
+}
